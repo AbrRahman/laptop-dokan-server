7a356dbb6f1676a77881aa233dd87b4fb430393a
AbrRahman <abrahman.est@gmail.com>
2022-11-29 09:45:15 +0600

    setup dbconnection

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 const port = process.env.PORT || 5000;
 // passing json
 app.use(express.json())
 app.use(cors())
 
+
+// mongodb connection string
+const uri = `mongodb+srv://${process.env.DD_USER}:${process.env.DB_PASSWORD}@cluster0.fotdr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+
+        // set all collection
+        const productsCollection = client.db("LaptopDokan").collection("products");
+        const categoriesCollection = client.db("LaptopDokan").collection("categories");
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+run().catch(err => {
+    console.log(err)
+})
+
 // setup routing
 app.get('/', (req, res) => {
     res.send('Hello world')
+    console.log(uri)
 })
 
 // set not found handlers
@@ -34,3 +55,4 @@ app.listen(port, (err) => {
 })
 
 
+// 00005C  3B185F
