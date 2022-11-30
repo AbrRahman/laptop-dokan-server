dfd734340de409f4e6780ac4c3584abaca428211
AbrRahman <abrahman.est@gmail.com>
2022-11-30 13:05:07 +0600

    save user db

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const run = async () => {
 
         // set all collection
         const productsCollection = client.db("LaptopDokan").collection("products");
-        const categoriesCollection = client.db("LaptopDokan").collection("categories")
-
+        const categoriesCollection = client.db("LaptopDokan").collection("categories");
+        const userCollection = client.db("LaptopDokan").collection("users");
         // setup routing
         app.get('/', (req, res) => {
             res.send('Hello world')
@@ -29,8 +29,40 @@ const run = async () => {
         // find categories
         app.get('/categories', async (req, res) => {
             const cursor = {};
-            const result = await categoriesCollection.find({}).toArray();
+            const result = await categoriesCollection.find(cursor).toArray();
             res.send(result)
+        })
+
+        // get categories product
+        app.get('/categories/:id', async (req, res) => {
+            const categoriesId = req.params.id;
+            const cursor = { category_id: categoriesId };
+            const result = await productsCollection.find(cursor).toArray();
+            res.send(result)
+        })
+
+        // get user 
+        app.get('/user/:id', async (req, res) => {
+            const email = req.params.id;
+            const exitingUser = await userCollection.findOne({ email: email });
+            res.send({
+                user: exitingUser
+            });
+        })
+        // create user
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            const exitingUser = await userCollection.findOne({ email: user.email });
+            if (exitingUser === null) {
+                const result = await userCollection.insertOne(user);
+                res.send(result)
+            } else {
+                res.send({
+                    acknowledged: false
+                })
+            }
+
+
         })
 
     } catch (err) {
